9473194d2417e0387e1efc311952d71e873ee8ae
Robert Onodi <robert.onodi89@gmail.com>
2015-10-21 12:51:14 +0300

    modified tests

--- tests/integration/authentication.test.js
+++ tests/integration/authentication.test.js
@@ -8,23 +8,26 @@ process.env.NODE_ENV = 'test';
 var http = require('http');
 var request = require('request');
 var chai = require('chai');
-var mongoose = require('mongoose');
-
-var app = require('../../server');
 var userFixture = require('../fixtures/user');
-
 var should = chai.should();
-var config = app.get('config');
-var baseUrl = config.baseUrl;
-var User = mongoose.model('User');
-var appServer;
 
 describe('Authentication', function() {
+  var mongoose;
+  var app;
+  var appServer;
+  var config;
+  var baseUrl;
+  var User;
 
   before(function(done) {
+    app = require('../../server');
+    config = app.get('config');
+    baseUrl = config.baseUrl;
     appServer = http.createServer(app);
 
     appServer.on('listening', function() {
+      mongoose = app.get('mongoose');
+      User = mongoose.model('User');
       User.create(userFixture, function(err, user) {
         if (err) throw err;
 
@@ -37,55 +40,53 @@ describe('Authentication', function() {
 
   after(function(done) {
     appServer.on('close', function() {
-      setTimeout(done, 200);
+      setTimeout(function() { done(); }, 1000);
     });
 
-    mongoose.connection.db.dropDatabase(function(err) {
+    User.remove({}).exec(function(err) {
       if (err) throw err;
 
-      mongoose.disconnect(function() {
+      mongoose.connection.close(function() {
         appServer.close();
       });
     });
   });
 
-  describe('Sign in user', function() {
-    it('should sign in a user with valid credentials', function(done) {
-      request({
-        method: 'POST',
-        url: baseUrl + '/auth/signin',
-        form: {
-          'email': userFixture.email,
-          'password': 'P@ssw0rd!'
-        },
-        json:true
-      }, function(err, res, body) {
-        if (err) throw err;
+  it('should sign in a user with valid credentials', function(done) {
+    request({
+      method: 'POST',
+      url: baseUrl + '/auth/signin',
+      form: {
+        'email': userFixture.email,
+        'password': 'P@ssw0rd!'
+      },
+      json:true
+    }, function(err, res, body) {
+      if (err) throw err;
 
-        res.statusCode.should.equal(200);
-        body.email.should.equal(userFixture.email);
-        should.not.exist(body.password);
-        should.not.exist(body.passwordSalt);
-        done();
-      });
+      res.statusCode.should.equal(200);
+      body.email.should.equal(userFixture.email);
+      should.not.exist(body.password);
+      should.not.exist(body.passwordSalt);
+      done();
     });
+  });
 
-    it('should not sign in a user with invalid credentials', function(done) {
-      request({
-        method: 'POST',
-        url: baseUrl + '/auth/signin',
-        form: {
-          'email': userFixture.email,
-          'password': 'incorrectpassword'
-        },
-        json:true
-      }, function(err, res, body) {
-        if (err) throw err;
+  it('should not sign in a user with invalid credentials', function(done) {
+    request({
+      method: 'POST',
+      url: baseUrl + '/auth/signin',
+      form: {
+        'email': userFixture.email,
+        'password': 'incorrectpassword'
+      },
+      json:true
+    }, function(err, res, body) {
+      if (err) throw err;
 
-        res.statusCode.should.equal(400);
-        body.message.should.equal('Invalid email or password.');
-        done();
-      });
+      res.statusCode.should.equal(400);
+      body.message.should.equal('Invalid email or password.');
+      done();
     });
   });
 
--- tests/integration/user.model.test.js
+++ tests/integration/user.model.test.js
@@ -8,139 +8,112 @@ process.env.NODE_ENV = 'test';
 var chai = require('chai');
 var should = chai.should();
 var config = require('../../config/environments/test');
-var mongoose = require('../../config/mongoose').init();
-var User = require('../../app/models/user');
 
-describe('User Model Integration', function() {
+describe('User model', function() {
+  var mongoose;
+  var User;
+  var _user;
+  var newUserData = {
+    email: 'register_user@test.com',
+    password: 'user_password',
+    name: 'New Test User'
+  };
+
+  before(function(done) {
+    mongoose = require('../../config/mongoose').init();
+    User = require('../../app/models/user');
+    done();
+  });
+
   after(function(done) {
-    mongoose.connection.db.dropDatabase(function(err) {
+    User.remove({}).exec(function(err) {
       if (err) throw err;
 
-      mongoose.disconnect(function() {
-        setTimeout(done, 200);
+      mongoose.connection.close(function() {
+        setTimeout(function() { done(); }, 1000);
       });
     });
   });
 
-  describe('#register()', function() {
-    var newUserData = {
-      email: 'register_user@test.com',
-      password: 'user_password',
-      name: 'New Test User'
-    };
+  it('should register a user', function(done) {
+    User.register(newUserData, function(err, user) {
+      if (err) throw err;
 
-    it('should create a new user', function(done) {
-      User.register(newUserData, function(err, user) {
-        if (err) throw err;
+      should.exist(user);
+      user.email.should.equal(newUserData.email);
+      should.not.exist(user.password);
+      should.not.exist(user.passwordSalt);
+      should.exist(user.createdAt);
+      user.active.should.equal(true);
 
-        should.exist(user);
-        user.email.should.equal(newUserData.email);
-        should.not.exist(user.password);
-        should.not.exist(user.passwordSalt);
-        should.exist(user.createdAt);
-        user.active.should.equal(true);
-        done();
-      });
-    });
-
-    it('should not create a new user if email already exists', function(done) {
-      User.register(newUserData, function(err, user) {
-        should.exist(err);
-        err.code.should.equal(11000); // duplicate key error
-        should.not.exist(user);
-        done();
-      });
+      _user = user;
+      done();
     });
   });
 
-  describe('#authenticate()', function() {
-    var _user;
-
-    before(function(done) {
-      User.register({
-        email: 'auth_user@test.com',
-        password: 'user_password',
-        name: 'New Test User'
-      }, function(err, user) {
-        if (err) throw err;
-
-        _user = user;
-        done();
-      });
+  it('should not register a user if already exists', function(done) {
+    User.register(newUserData, function(err, user) {
+      should.exist(err);
+      err.code.should.equal(11000); // duplicate key error
+      should.not.exist(user);
+      done();
     });
+  });
 
-    it('should return the user if the credentials are valid', function(done) {
-      User.authenticate(_user.email, 'user_password', function(err, user) {
+  it('should authenticate a user with valid credentials', function(done) {
+    User.authenticate(newUserData.email, 'user_password', function(err, user) {
+      if (err) throw err;
+
+      should.exist(user);
+      should.not.exist(user.password);
+      should.not.exist(user.passwordSalt);
+      user.email.should.equal(newUserData.email);
+      done();
+    });
+  });
+
+  it('should not authenticate user with invalid credentials', function(done) {
+    User.authenticate(newUserData.email, 'notuserpassowrd', function(err, user) {
+      if (err) throw err;
+
+      should.not.exist(user);
+      done();
+    });
+  });
+
+  it('should change the password of a user', function(done) {
+    _user.changePassword('user_password', 'new_user_password', function(err, result) {
+      if (err) throw err;
+
+      should.exist(result);
+      result.success.should.equal(true);
+      result.message.should.equal('Password changed successfully.');
+      result.type.should.equal('password_change_success');
+
+      // run a check credential with the new password
+      User.authenticate(_user.email, 'new_user_password', function(err, user) {
         if (err) throw err;
 
         should.exist(user);
-        should.not.exist(user.password);
-        should.not.exist(user.passwordSalt);
         user.email.should.equal(_user.email);
         done();
       });
     });
-
-    it('should return nothing if the credential of the user are invalid', function(done) {
-      User.authenticate(_user.email, 'notuserpassowrd', function(err, user) {
-        if (err) throw err;
-
-        should.not.exist(user);
-        done();
-      });
-    });
   });
 
-  describe('#changePassword()', function() {
-    var _user;
+  it('should not change password if old password does not match', function(done) {
+    _user.changePassword('not_good', 'new_user_password', function(err, result) {
+      should.not.exist(result);
+      should.exist(err);
+      err.type.should.equal('old_password_does_not_match');
 
-    before(function(done) {
-      User.register({
-        email: 'changePassword_user@test.com',
-        password: 'user_password',
-        name: 'New Test User'
-      }, function(err, user) {
+      // run a check credential with the old password
+      User.authenticate(_user.email, 'new_user_password', function(err, user) {
         if (err) throw err;
 
-        _user = user;
+        should.exist(user);
+        user.email.should.equal(_user.email);
         done();
-      });
-    });
-
-    it('should change the password of a user', function(done) {
-      _user.changePassword('user_password', 'new_user_password', function(err, result) {
-        if (err) throw err;
-
-        should.exist(result);
-        result.success.should.equal(true);
-        result.message.should.equal('Password changed successfully.');
-        result.type.should.equal('password_change_success');
-
-        // run a check credential with the new password
-        User.authenticate(_user.email, 'new_user_password', function(err, user) {
-          if (err) throw err;
-
-          should.exist(user);
-          user.email.should.equal(_user.email);
-          done();
-        });
-      });
-    });
-
-    it('should not change password if old password does not match', function(done) {
-      _user.changePassword('not_good', 'new_user_password', function(err, result) {
-        should.not.exist(result);
-        should.exist(err);
-        err.type.should.equal('old_password_does_not_match');
-
-        // run a check credential with the old password
-        User.authenticate(_user.email, 'new_user_password', function(err, user) {
-          if (err) throw err;
-
-          should.exist(user);
-          user.email.should.equal(_user.email);
-          done();
-        });
       });
     });
   });
