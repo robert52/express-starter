b22e1e8f45eeb4350840ff464d87eb26f1f11404
Robert Onodi <robert.onodi89@gmail.com>
2015-10-21 12:50:59 +0300

    connection cleanup on end

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,13 +3,25 @@
 var mongoose = require('mongoose');
 var config = require('./index');
 
-module.exports.init = function(app) {
+module.exports.init = init;
+
+function init(app) {
   mongoose.connect(config['mongodb'].uri);
 
-  // add the mongoose object to the app instance
+  // If the Node process ends, cleanup existing connections
+  process.on('SIGINT', cleanup);
+  process.on('SIGTERM', cleanup);
+  process.on('SIGHUP', cleanup);
+
   if (app) {
     app.set('mongoose', mongoose);
   }
 
   return mongoose;
 };
+
+function cleanup() {
+  mongoose.connection.close(function () {
+    process.exit(0);
+  });
+}
